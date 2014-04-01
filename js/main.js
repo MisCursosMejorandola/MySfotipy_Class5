18873e3526ab3c055721c953493df65c7729cee7
andacosta <and_acosta@hotmail.com>
2014-04-01 09:40:50 -0500

    Avances Clase 5
    Hasta minuto 59 de la clase

--- js/main.js
+++ js/main.js
@@ -5,16 +5,20 @@ Sfotipy.Song = Backbone.Model.extend({});
 Sfotipy.SongView = Backbone.View.extend({
 	tagName: 'li',		// Lista no ordenada
 	className: 'item border-bottom',
+	// Jalamos el template html
+	template: Handlebars.compile($("#song-template").html()),
 
 	render: function() {
-		var song = this.model;
-		var name = song.get('name');
-		var author = song.get('author');
+		// var song = this.model;
+		// var name = song.get('name');
+		// var author = song.get('author');
 
 		// Instanciamiento jQuery en Backbone para un elemento html
-		this.$el.html("<span>" + author + "</span> - <span>" + name + "</span>");
+		// this.$el.html("<span>" + author + "</span> - <span>" + name + "</span>");
+		var html = this.template(this.model.toJSON());
+		this.$el.html(html);
 	}
 });
 
-// Lo instanciamos globalmente
+// Instanciamos el objeto globalmente
 window.Sfotipy = Sfotipy;
