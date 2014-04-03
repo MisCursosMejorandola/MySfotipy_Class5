6fe5c7e8473f2a62fd70b6d26e707a11b35d8df7
andacosta <and_acosta@hotmail.com>
2014-04-03 06:33:16 -0500

    New changes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
+// Definicion del namespace
 Sfotipy = {};
 
 Sfotipy.Song = Backbone.Model.extend({});
+
+// Coleccion
+Sfotipy.Songs = Backbone.Collection.extend({
+	model: Sfotipy.Song
+});
 
 Sfotipy.SongView = Backbone.View.extend({
 	// Captura de eventos - Clic en Botones
@@ -10,19 +16,53 @@ Sfotipy.SongView = Backbone.View.extend({
 	},
 
 	tagName: 'li',		// Lista no ordenada
+	
 	className: 'item border-bottom',
+	
 	// Jalamos el template html
 	template: Handlebars.compile($("#song-template").html()),
 
+	// Renderizacion automatica cuando cambia el modelo
+	initialize: function() {
+		this.listenTo(this.model, "change", this.render, this);
+	},
+
+	// Renderiza la vista para actualizar los cambios
 	render: function() {
 		var html = this.template(this.model.toJSON());
 		this.$el.html(html);
-	}
+	},
 
 	add: function (e) {
-		alert:(this.model.get("name"));
-	},
+		alert(this.model.get("name"));
+	}
 });
+
+// Rutas de la aplicacion
+Sfotipy.Router = Backbone.Router.extend({
+	routes: {
+		"": "index",
+		"album/:name": "album",
+		"profile/:username": "profile"
+	},
+
+	// Ruta por defecto
+	index: function() {
+		console.log("Estoy en el index");
+	},
+
+	album: function() {
+		console.log("Album" + name);
+	},
+
+	profile: function() {
+		console.log("Username" + username);
+	}
+});
+
+// Instanciamiento del enrutador
+Sfotipy.app = new Sfotipy.Router();
+Backbone.history.start();
 
 // Instanciamos el objeto globalmente
 window.Sfotipy = Sfotipy;
